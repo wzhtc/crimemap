593911c332c25812351d92ca05adaa80587497cf
zhonghao wu <zhonghao wu>
2017-08-13 23:31:05 -0400

    f

--- dbhelper.py
+++ dbhelper.py
@@ -13,30 +13,32 @@ class DBHelper:
     def get_all_inputs(self):
         connection = self.connect()
         try:
-        	query = "SELECT description FROM crimes;"
-	        with connection.cursor() as cursor:
-	        cursor.execute(query)
-	        return cursor.fetchall()
+            query = "SELECT description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                return cursor.fetchall()
         finally:
-        	connection.close()
+            connection.close()
 
     def add_input(self, data):
         connection = self.connect()
         try:
-            # The following introduces a deliberate security flaw. See section on SQL injection below
-        	query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
-	        with connection.cursor() as cursor:
-	        cursor.execute(query)
-	        connection.commit()
+            # The following introduces a deliberate security flaw. See section
+            # on SQL injection below
+            query = "INSERT INTO crimes (description) VALUES ('{}');".format(
+                data)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
         finally:
-        	connection.close()
+            connection.close()
 
     def clear_all(self):
         connection = self.connect()
         try:
-	        query = "DELETE FROM crimes;"
-	        with connection.cursor() as cursor:
-	        cursor.execute(query)
-	        connection.commit()
+            query = "DELETE FROM crimes;"
+            with connection.cursor() as cursor:
+	            cursor.execute(query)
+	            connection.commit()
         finally:
-        	connection.close()
+            connection.close()
